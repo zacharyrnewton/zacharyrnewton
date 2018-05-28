9f436cd3482f2314308bc5e9e298dbf164451fc5
Zachary R Newton <zacharyrnewton@MacBook-Pro-13-2014.local>
2018-05-27 22:26:57 -0400

    optimization

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -45,7 +45,7 @@
   setInterval(themeSet, 5000)
 })();
 
-//(function() {
-//  var heroVideo = new URL(document.querySelector('.heroVideo').href).searchParams.get('v');
-//  document.querySelector('.heroVideo').innerHTML = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/" + heroVideo + "?rel=0&amp;controls=0&amp;showinfo=0\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen></iframe>"
-//})();
+(function() {
+  var heroVideo = new URL(document.querySelector('.heroVideo').href).searchParams.get('v');
+  document.querySelector('.heroVideo').innerHTML = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/" + heroVideo + "?rel=0&amp;controls=0&amp;showinfo=0\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen></iframe>"
+})();
