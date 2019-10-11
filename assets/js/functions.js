03da5dd4a60c00fb40fcdb016fc2353f5f91ee3b
Zachary Newton <zacharyrnewton@gmail.com>
2019-10-11 00:25:32 -0400

    Updated css var pref based on user luminosity pref

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -11,39 +11,39 @@
   });
 })();
 
-(function themeSet() {
-  var time = new Date().getHours();
-  var root = document.querySelector(':root');
-  if (time >= 21 || time <= 6) {
-    root.style.setProperty("--white","#000000");
-    root.style.setProperty("--black","#FFFFFF");
-    root.style.setProperty("--whiteTwo","#1A1A1F");
-    root.style.setProperty("--whiteThree","#FFFFFF");
-    root.style.setProperty("--lightOne","#0F0F12");
-    root.style.setProperty("--lightTwo","#19191F");
-    root.style.setProperty("--darkOne","#F5F5F7");
-    root.style.setProperty("--darkTwo","#E8E8ED");
-    root.style.setProperty("--secondaryLight","#8A8A99");
-    root.style.setProperty("--secondaryDark","#1A1A1F");
-    root.style.setProperty("--youtubeRed","#FF0000");
-    root.style.setProperty("--patreonOrange","#F96854");
-  }
-  else {
-    root.style.setProperty("--white","#FFFFFF");
-    root.style.setProperty("--black","#000000");
-    root.style.setProperty("--whiteTwo","#FFFFFF");
-    root.style.setProperty("--whiteThree","#FFFFFF");
-    root.style.setProperty("--lightOne","#F5F5F7");
-    root.style.setProperty("--lightTwo","#E8E8ED");
-    root.style.setProperty("--darkOne","#0F0F12");
-    root.style.setProperty("--darkTwo","#19191F");
-    root.style.setProperty("--secondaryLight","#8A8A99");
-    root.style.setProperty("--secondaryDark","#1A1A1F");
-    root.style.setProperty("--youtubeRed","#FF0000");
-    root.style.setProperty("--patreonOrange","#F96854");
-  }
-  setInterval(themeSet, 5000)
-})();
+// (function themeSet() {
+//   var time = new Date().getHours();
+//   var root = document.querySelector(':root');
+//   if (time >= 21 || time <= 6) {
+//     root.style.setProperty("--white","#000000");
+//     root.style.setProperty("--black","#FFFFFF");
+//     root.style.setProperty("--whiteTwo","#1A1A1F");
+//     root.style.setProperty("--whiteThree","#FFFFFF");
+//     root.style.setProperty("--lightOne","#0F0F12");
+//     root.style.setProperty("--lightTwo","#19191F");
+//     root.style.setProperty("--darkOne","#F5F5F7");
+//     root.style.setProperty("--darkTwo","#E8E8ED");
+//     root.style.setProperty("--secondaryLight","#8A8A99");
+//     root.style.setProperty("--secondaryDark","#1A1A1F");
+//     root.style.setProperty("--youtubeRed","#FF0000");
+//     root.style.setProperty("--patreonOrange","#F96854");
+//   }
+//   else {
+//     root.style.setProperty("--white","#FFFFFF");
+//     root.style.setProperty("--black","#000000");
+//     root.style.setProperty("--whiteTwo","#FFFFFF");
+//     root.style.setProperty("--whiteThree","#FFFFFF");
+//     root.style.setProperty("--lightOne","#F5F5F7");
+//     root.style.setProperty("--lightTwo","#E8E8ED");
+//     root.style.setProperty("--darkOne","#0F0F12");
+//     root.style.setProperty("--darkTwo","#19191F");
+//     root.style.setProperty("--secondaryLight","#8A8A99");
+//     root.style.setProperty("--secondaryDark","#1A1A1F");
+//     root.style.setProperty("--youtubeRed","#FF0000");
+//     root.style.setProperty("--patreonOrange","#F96854");
+//   }
+//   setInterval(themeSet, 5000)
+// })();
 
 (function() {
   var heroVideo = new URL(document.querySelector('.heroVideo').href).searchParams.get('v');
